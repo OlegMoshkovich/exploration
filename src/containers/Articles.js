659cd77af5e74a089756425648273f75dcf9630c
Oleg Moshkovich <omoshkovich@gmail.com>
2019-10-22 17:01:14 -0400

    fetching using await

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -3,16 +3,14 @@ import React, { useEffect, useState } from 'react';
 export const Articles = () => {
     const key = 'mnnfCWd6y4tQ4IipGbUkIZXICpFqbHH1'
     const [state, setState] = useState(('hello'))
-    const [response, setResponse] = useState('')
     useEffect(() => {
-        function fetchArticles() {
-            fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=trump&api-key=${key}`)
-                .then(resp => resp.json())
-                .then((data) => console.log('data from the chain', data))
+        async function fetchData() {
+            const response = await fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=trump&api-key=${key}`);
+            const articles = await response.json()
+            console.log('articles', articles)
         }
-        fetchArticles()
+        fetchData()
     }, [])
-
     return (
         <div>
             {state}
