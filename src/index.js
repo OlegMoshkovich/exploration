0c811949443173d8071f3658240e8fed4ee566b2
Oleg Moshkovich <omoshkovich@gmail.com>
2019-11-11 13:24:00 -0500

    added the https redirect

--- src/index.js
+++ src/index.js
@@ -18,33 +18,32 @@ import { TypeMessage } from "./containers/typeWriter"
 import { Articles } from "./containers/Articles"
 import { Hover } from "./containers/hover"
 import { mapThree } from './containers/mapThree'
-
-
+import HttpsRedirect from 'react-https-redirect';
 
 
 const store = createStore(experimentApp);
 
 const routing = (
-  <Provider store={store}>
-    <Router>
-      <div>
-        <Route exact path="/" component={App} />
-        <Route exact path="/map" component={Map} />
-        <Route exact path="/mapexperiment" component={MapExperiment} />
-        <Route exact path="/maptrips" component={MapTrip} />
-        <Route exact path="/tiny" component={Tiny} />
-        <Route exact path="/material" component={Material} />
-        <Route exact path="/graph" component={graphViz} />
-        <Route exact path="/form" component={Basic} />
-        <Route exact path="/type" component={TypeMessage} />
-        <Route exact path="/articles" component={Articles} />
-        <Route exact path="/hover" component={Hover} />
-        <Route exact path="/mapthree" component={mapThree} />
-
-
-      </div>
-    </Router>
-  </Provider>
+  <HttpsRedirect>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route exact path="/" component={App} />
+          <Route exact path="/map" component={Map} />
+          <Route exact path="/mapexperiment" component={MapExperiment} />
+          <Route exact path="/maptrips" component={MapTrip} />
+          <Route exact path="/tiny" component={Tiny} />
+          <Route exact path="/material" component={Material} />
+          <Route exact path="/graph" component={graphViz} />
+          <Route exact path="/form" component={Basic} />
+          <Route exact path="/type" component={TypeMessage} />
+          <Route exact path="/articles" component={Articles} />
+          <Route exact path="/hover" component={Hover} />
+          <Route exact path="/mapthree" component={mapThree} />
+        </div>
+      </Router>
+    </Provider>
+  </HttpsRedirect>
 );
 ReactDOM.render(routing, document.getElementById("root"));
 serviceWorker.unregister();
