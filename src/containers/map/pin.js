8d53bbf09c416ad60df220f35a3bda3d6492ad61
Oleg Moshkovich <omoshkovich@gmail.com>
2019-11-29 22:42:42 -0500

    adjusted the size of the marker

--- src/containers/map/pin.js
+++ src/containers/map/pin.js
@@ -8,7 +8,7 @@ const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,
 
 const Pin = (props) => {
 
-  const { size = 20, onClick, color, outline } = props;
+  const { size = 25, onClick, color, outline } = props;
   const pinStyle = {
     fill: color || "#FF00D6",
     stroke: outline || "#B7F1FD",
