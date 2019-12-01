5eb46ed3c80212adbdc1e666c009bd8f34e00fc1
Oleg Moshkovich <omoshkovich@gmail.com>
2019-11-30 19:04:37 -0500

    added svg icons to the menu

--- src/containers/map/toggle.js
+++ src/containers/map/toggle.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 import Draggable from "react-draggable";
-import { ToggleCircle } from "./styles";
+import { ToggleCircle, Image } from "./styles";
+
 
 export const Toggle = (props) => {
   const [state, setState] = useState('true');
@@ -20,7 +21,16 @@ export const Toggle = (props) => {
         color1={props.color1}
         toggle={state}
         onClick={toggle}
-      />
-    </Draggable>
+      >
+        <svg x="0px" y="0px"
+          viewBox="0 0 100 101.48"  >
+          <g>
+            <rect x="12.05" y="27.07" width="75.9" height="5" />
+            <rect x="12.05" y="47.5" width="75.9" height="5" />
+            <rect x="12.05" y="67.93" width="75.9" height="5" />
+          </g>
+        </svg>
+      </ToggleCircle>
+    </Draggable >
   );
 };
